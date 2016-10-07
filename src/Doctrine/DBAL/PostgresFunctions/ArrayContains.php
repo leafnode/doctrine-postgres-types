2a10c6c05d49fa16dedc4c60418a1a6bf1d3d65b
Leszek Krupiński <leafnode@gmail.com>
2016-10-08 00:58:28 +0200

    overcoming doctrine limitations

---
+++ src/Doctrine/DBAL/PostgresFunctions/ArrayContains.php
@@ -0,0 +1,41 @@
+<?php
+namespace Doctrine\DBAL\PostgresFunctions;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\SqlWalker;
+
+class ArrayContains extends FunctionNode
+{
+    protected $operator;
+    protected $first;
+    protected $second;
+
+    /**
+    * @param SqlWalker $sqlWalker
+    *
+    * @return string
+    */
+    public function getSql(SqlWalker $sqlWalker)
+    {
+        return sprintf("%s <@ %s",
+        $this->first->dispatch($sqlWalker),
+        $this->second->dispatch($sqlWalker)
+        );
+    }
+    /**
+    * @param Parser $parser
+    *
+    * @return void
+    */
+    public function parse(Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $this->first = $parser->ArithmeticPrimary();
+        $parser->match(Lexer::T_COMMA);
+        $this->second = $parser->ArithmeticPrimary();
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+}
